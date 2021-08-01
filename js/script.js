134cec6e06567fa18e15a5883e172cdbc49eeb39
Djones106 <djones106@gmail.com>
2021-07-31 21:20:49 -0400

    Updated script.js added quotes

--- js/script.js
+++ js/script.js
@@ -10,13 +10,32 @@ Project 4 - Random Quote Generator in JavaScript
 /*** 
  * `quotes` array 
 ***/
+var quotes ={ quotes: [
+  {quote: "Venture outside your comfort zone. The rewards are worth it.", source: "Disney - Rapunzel", citation: "Disney - Tangled", year: "2010"},
 
+  {quote: "A little consideration, a little thought for others, makes all the difference.", source: "Eeyore", citation: "Disney - Winnie the Pooh", year: "1926"},
 
+  {quote: "The flower that blooms in adversity is the most rare and beautiful of all.", source: "The Emperor", citation: "Disney - Mulan", year: "1988"},
 
+  {quote: "In every job that must be done, there is an element of fun.", source: "Mary Poppins", citation: "Disney - Marry Poppins", year: "1964"},
+
+  {quote: "The only thing predictable about life is its unpredictability.", source: "Remy", citation: "Disney - Ratatouille", year: "2007"},
+
+  {quote: "Love is putting someone else's needs before yours.", source: "Olaf", citation: "Disney - Frozen", year: "2013"},
+
+  {quote: "All our dreams can come true, if we have the courage to pursue them.", source: "Walt Disney", citation: "", year: ""},
+
+  {quote: "I want adventure in the great wide somewhere. I want it more than I can tell. And for once it might be grand, to have someone understand, I want so much more than they have planned.", source: "Belle", citation: "Disney - Beauty and the Beast", year: "1991"}
+]}; 
+
+//console.log(quotes)
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote(){
 
+
+}
 
 
 /***
